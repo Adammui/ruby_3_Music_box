ea0fe6e9759a2d200d071448ed52f7b5b8eac55a
Adammui <adami.yuje@gmail.com>
2021-11-16 19:29:51 +0300

    ?

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,7 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+a = Artist.create([{ group_name: 'affinage', country: 'ru' }, { group_name: 'picnic', country: 'ru' }])
+
+Album.create(album_name: 'aff', release_date: '12.12.2001', artist_id: 1)
+Song.create([{song_name:"Vano", genre:'Rock, Folk', album_id:1},{song_name:"Kotik", genre:'Rock, Folk', album_id:1}])
